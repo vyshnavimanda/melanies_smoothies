ac86385795e7a67e8bd97544c47ea549c219dd6c
vyshnavimanda <63095235+vyshnavimanda@users.noreply.github.com>
2024-11-27 15:58:42 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,7 +35,7 @@ except Exception as e:
 # Fetch data from Snowflake and convert to DataFrame
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON')).collect()
 # st.dataframe(data=my_dataframe, use_container_width=True)
-my_dataframe = pd.DataFrame(my_dataframe)
+# my_dataframe = pd.DataFrame(my_dataframe)
 # st.stop()
 
 #convert snowpark dataframe to pandas dataframe so we can use LOC function
