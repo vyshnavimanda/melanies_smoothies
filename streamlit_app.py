87a3e684007c17e6cdadcdac9d876c06d792a30a
vyshnavimanda <63095235+vyshnavimanda@users.noreply.github.com>
2024-11-27 16:00:38 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,7 +39,7 @@ my_dataframe = pd.DataFrame(my_dataframe)
 # st.stop()
 
 #convert snowpark dataframe to pandas dataframe so we can use LOC function
-# pd_df=my_dataframe.to_pandas()
+pd_df=my_dataframe.to_pandas()
 st.dataframe(pd_df)
 st.stop()
 
