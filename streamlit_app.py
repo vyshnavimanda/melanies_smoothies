19ab4185dcb5f5e9de3c190c703f99c8c85df65a
vyshnavimanda <63095235+vyshnavimanda@users.noreply.github.com>
2024-11-27 15:54:40 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,9 +34,9 @@ except Exception as e:
 
 # Fetch data from Snowflake and convert to DataFrame
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON')).collect()
-st.dataframe(data=my_dataframe, use_container_width=True)
+# st.dataframe(data=my_dataframe, use_container_width=True)
 my_dataframe = pd.DataFrame(my_dataframe)
-st.stop()
+# st.stop()
 
 # Extract fruit names for multiselect
 fruit_names = my_dataframe['FRUIT_NAME'].tolist()
